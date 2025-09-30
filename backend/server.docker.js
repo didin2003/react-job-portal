fda001880c7ddfb7c6372da4bf39b6faf2ec55ea
Didin <didinpg8080@gmail.com>
2025-09-30 04:02:45 +0000

    updated

--- backend/server.docker.js
+++ backend/server.docker.js
@@ -1,5 +1,19 @@
 import app from "./app.docker.js";
 import cloudinary from "cloudinary";
+import promBundle from "express-prom-bundle";
+
+const metricsMiddleware = promBundle({
+    includeMethod: true, // Track HTTP methods
+    includePath: true, // Track request paths
+    includeStatusCode: true, // Track response codes
+    metricsPath: "/metrics", // Prometheus will scrape here
+    promClient: {
+	collectDefaultMetrics: {}, // CPU, memory, event loop lag, etc.
+    },
+});
+
+app.use(metricsMiddleware);
+
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -11,4 +25,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
+  console.log(`Prometheus metrics available at http://localhost:${PORT}/metrics`);
 });
